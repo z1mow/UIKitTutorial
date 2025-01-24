d975ec93798ec9c90d83564e9e038389a7183bee
Şakir Yılmaz Öğüt <zimowcs@gmail.com>
2025-01-24 15:01:31 +0300

    Initial Commit

---
+++ UIKitTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitTutorial
+//
+//  Created by Şakir Yılmaz ÖĞÜT on 24.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
